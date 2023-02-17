87dff0c95d97eb47928fdcf100e2478cc992068c
Begzod <103951263+Begzod2004@users.noreply.github.com>
2023-02-17 23:39:48 +0500

    added admin panel

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Comments)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['name','comment','image','published']
+    list_filter = ['published']
+    search_fields = ["id"]
+
+
+@admin.register(MyResume)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['file']
+    list_filter = ['file']
+    search_fields = ["file"]
+
+
+@admin.register(MyProjects)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['category','side','image','title','body','link','published']
+    list_filter = ['category']
+    search_fields = ["id"]
+
+
+@admin.register(MySkills)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['name','image','published']
+    list_filter = ['published']
+    search_fields = ["id"]
+
+
+@admin.register(MyWorkExprea)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['name','image','start_date','finish_date','published']
+    list_filter = ['start_date','finish_date']
+    search_fields = ["id"]
+
+
+@admin.register(GetInTouch)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['fullname','email','body','published']
+    list_filter = ['published']
+    search_fields = ["id"]
+
+
+
+
